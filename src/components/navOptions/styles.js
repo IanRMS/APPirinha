a8d1231c7a8da07df097b8256fd757bc80acf538
Ian Ribeiro <ian@smn.com.br>
2020-05-13 08:54:59 -0300

    refeat: navoptions responsive tip

--- src/components/navOptions/styles.js
+++ src/components/navOptions/styles.js
@@ -45,6 +45,10 @@ export const NavOption = styled.button`
     opacity: 0;
     overflow: hidden;
     transition: 180ms ease-in-out;
+
+    @media (max-width: 768px) {
+      display: none;
+    }
   }
 
   :hover {
